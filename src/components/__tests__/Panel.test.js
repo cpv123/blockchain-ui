0e30e40e500038892a31b3c3b7099a132e14773d
cpv123 <c.vibert@hotmail.com>
2018-12-11 19:22:24 +0000

    Better snapshot test for components

--- src/components/__tests__/Panel.test.js
+++ src/components/__tests__/Panel.test.js
@@ -2,39 +2,25 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Panel from '../Panel';
 
-const mockProps = {
-    title: "Test Panel",
-    children: {},
-};
+describe('The Panel component', () => {
 
-describe('the Panel component', () => {
-
-    it('renders correctly', () => {
-        const wrapper = shallow(<Panel {...mockProps} />);
-        expect(wrapper).toMatchSnapshot();
-    });
-
-    it('renders children correctly', () => {
+    it('renders as expected with children', () => {
+        const props = { title: "Test Panel" };
         const wrapper = shallow(
-            <Panel title={mockProps.title}>
+            <Panel {...props}>
                 <div>
                     <p>Test panel children</p>
                 </div>
             </Panel>
-        );
-        expect(wrapper.find("div")).toHaveLength(1);
-        expect(wrapper.find("p")).toHaveLength(1);
-        expect(wrapper.find("p").text()).toEqual("Test panel children");
-    });
+        ).dive();
 
-    it('renders the title prop correctly', () => {
-        const wrapper = shallow(<Panel {...mockProps} />);
-        expect(wrapper.prop("title")).toEqual("Test Panel");
+        expect(wrapper).toMatchSnapshot();
     });
 
     it('handles no children', () => {
-        const wrapper = shallow(<Panel title={mockProps.title} />);
+        const props = { title: "Test Panel" };
+        const wrapper = shallow(<Panel {...props} />).dive();
+
         expect(wrapper.exists()).toBe(true);
     });
 })
-
--- src/utils/__tests__/buildUrl.test.js
+++ src/utils/__tests__/buildUrl.test.js
@@ -1,10 +1,10 @@
 import buildUrl from "../buildUrl";
 import { baseUrl } from "../buildUrl";
 
-describe('the buildUrl function', () => {
+describe('The buildUrl function', () => {
 
     it('should return the correct URL given a path', () => {
-        const path ="test/path";
+        const path = "test/path";
         expect(buildUrl(path)).toBe("http://0.0.0.0:5000/test/path");
     });
 
