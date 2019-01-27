90653d6168b4fab69fd9aec371c6836972585946
cpv123 <35846795+cpv123@users.noreply.github.com>
2019-01-27 22:30:00 +0000

    Update FullChain.js

--- src/features/full-chain/FullChain.js
+++ src/features/full-chain/FullChain.js
@@ -6,25 +6,14 @@ function FullChain({ chainData }) {
     return (
         <div>
             {
-                chainData.chain.map(block => {
-                    if (block.index === 1) {
-                        return (
-                            <Panel title="Genesis Block" color={'white'}>
-                                <BlockDetails
-                                    block={block}
-                                />
-                            </Panel>
-                        );
-                    } else {
-                        return (
-                            <Panel title={`Block ${block.index}`} color={'white'}>
-                                <BlockDetails
-                                    block={block}
-                                />
-                            </Panel>
-                        );
-                    }
-                })
+                chainData.chain.map(block => (
+                    <Panel 
+                        title={block.index !== 1 ? `Block ${block.index}` : "Genesis Block"} 
+                        color={'white'}
+                    >
+                        <BlockDetails block={block} />
+                    </Panel>
+                ))
             }
         </div>
     );
